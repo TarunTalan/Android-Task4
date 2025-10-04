d9a7a90a1642d85bba691a0d957790a75f8b09c1
TarunTalan <taruntalan12@gmail.com>
2025-10-04 11:09:41 +0530

    init recyclerview in Activity

--- app/src/main/java/com/example/myapp/CreatorChannelActivity.kt
+++ app/src/main/java/com/example/myapp/CreatorChannelActivity.kt
@@ -1,4 +1,33 @@
 package com.example.myapp
 
-class CreatorChannelActivity {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat.enableEdgeToEdge
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapp.databinding.ActivityMainBinding
+
+class CreatorChannelActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val channelList = listOf(
+        }
+
+        val adapter = MyAdapter(channelList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.hasFixedSize()
+
+    }
 }
