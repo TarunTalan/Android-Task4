b0a7cd4c59e140c92191a9a63cc11994f965de1f
TarunTalan <taruntalan12@gmail.com>
2025-10-04 15:40:11 +0530

    improve ui

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapp.databinding.ActivityMainBinding
 
